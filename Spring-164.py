a10618238799df4e0288a1c50ef5554ce2db1479
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-03-19 18:41:17 +0000

    Update Spring-164.py

--- Spring-164.py
+++ Spring-164.py
@@ -517,12 +517,13 @@ class compression:
                                         Save=""
                                         Save_V=""
                                         Times_of_compression+=1
-                                        
+                                        if  len(sda4)>len(sda2):
+                                        	sda4="11111111"+sda3                                        
                                         if len(sda4)<=7920 or Times_of_compression==65535:
                                             Times_count=format(Times_of_compression,'016b')
                                             sda4=Times_count+sda4
                                             assxw1=1
-                                     
+ 
                                         sda2=sda4
                                         #print(len(sda4))
 
