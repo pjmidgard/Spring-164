381a4cbbaf882119309db9b165c4f94f58a4f52f
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-03-19 18:38:13 +0000

    Update Spring-164.py

--- Spring-164.py
+++ Spring-164.py
@@ -522,8 +522,7 @@ class compression:
                                             Times_count=format(Times_of_compression,'016b')
                                             sda4=Times_count+sda4
                                             assxw1=1
-                                        if  len(sda4)>len(sda2):
-                                        	sda4="11111111"+sda3 
+                                     
                                         sda2=sda4
                                         #print(len(sda4))
 
