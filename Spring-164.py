db59c9d32a67b612bcd895a51d240121db0d302a
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-03-23 08:06:48 +0000

    Update Spring-164.py

--- Spring-164.py
+++ Spring-164.py
@@ -86,6 +86,8 @@ class compression:
                         # Read the whole file at once
                         
                         data = binary_file.read()
+                        import paq
+                        data= paq.decompress(data)
                         
                         s=str(data)
                         
@@ -179,104 +181,14 @@ class compression:
                                             sda="0"+sda
                                             z=z+1
 
-                                    if count_time_of_copression==0:
-                                        sda9=sda
-                                        count4=int(sda[:8],2)
-                                        sda=sda[8:]
-                                        #print(count4)
-                                        Times_of_compression=int(sda[:16],2)
-                                        sda=sda[16:]
-                                        
-                                        #print(Times_of_compression)
-                                        count_time_of_copression=1
+
                                     
                                     sda2=sda
 
                                     lenf3=len(sda2)
-                                lenf2=len(sda2)  
-                                block3=0
-                                sda3=""
-
-                                
-                                Save=sda2[:8]
-                                sda2=sda2[8:]
-                               
-                                Tranformation=0
-                                combinations=int(Save,2)
-                                #print(combinations)
-                                
-                                if combinations==255:
-                                    Tranformation=1
-                                    
-                                if Tranformation==0:
-                                    combinations1=combinations+1
-                                    combinations1_bytes=combinations1*8
-                                    Save_V=sda2[:combinations1_bytes]
-                                    sda2=sda2[combinations1_bytes:]
-
-
-                                    size_data3=sda2
-                                    if size_data3[0:9]=="000000001":
-                                        sda2=sda2[9:]
-                                    elif size_data3[0:8]=="00000001":
-                                        sda2=sda2[8:]
-                                    elif size_data3[0:7]=="0000001":
-                                        sda2=sda2[7:]
-                                    elif size_data3[0:6]=="000001":
-                                        sda2=sda2[6:]
-                                    elif size_data3[0:5]=="00001":
-                                        sda2=sda2[5:]
-                                    elif size_data3[0:4]=="0001":
-                                        sda2=sda2[4:]
-                                    elif size_data3[0:3]=="001":
-                                        sda2=sda2[3:]
-                                    elif size_data3[0:2]=="01":
-                                        sda2=sda2[2:]
-                                    elif size_data3[0:1]=="1":
-                                        sda2=sda2[1:]
-
-
-                                
-                                sda3=sda2
-                                
-
-                                if Tranformation==1:
-
-                                            sda4=sda3
-                                            #print(sda4)
-                                if Tranformation==0:
-                                            G=bin(combinations)[2:]
-                                            F=len(G)
-                                            C="0"+str(F)+"b"
-                                            #print(C)
-                                            Bits=0
-                                            block3=0
-                                            sda4=""
-                                            lenf3=len(sda3)
-                                            while block3<lenf3:
-                                                e4=sda3[block3:block3+F]
-                                                e5=""
-                                                
-                                                Bits1=0
-                                                Bits=0
-                                                while e4!=e5:
-                                                    
-                                                    e5=Save_V[Bits1:Bits1+8]
-                                                    Bits1+=8
-                                                    Bits+=1
-                                                    
-                                                if e4==e5:
-                                                    sda4=sda4+e5
-                                                    block3+=F
-                                    
-                                
-                                   
-                                    
-                                Times_of_compression1+=1
-                                assxw=assxw+1
-                                assxw1=0
-                                sda2=sda4
-                                #print(sda2)
+                                lenf2=len(sda2)
+                                #print(len(sda2))
+                                assxw=1
                                 if assxw==1:
                                             assxw1=0
                                             #print(count4)
@@ -374,6 +286,8 @@ class compression:
                                                             
                                                             #print(e11)
                                                         sda3+=e6
+                                                        if count4==4:
+                                                           count4=1	 
                                                        
                                                         #print(count4)
                                                         block3+=8
@@ -382,14 +296,14 @@ class compression:
 	                                            
 	                        
 	                                            #print(len(sda3))
-                                                    if count4==1:
-                                                           count4=4	                                            
+                                                                                          
 	                                              
 	                                            #print(sda2)
 	                                            
                                                     sda2=sda3
                                                     #print(sda2)
                                                     assxw1=assxw1+1
+                                                    #print(assxw1)
                                                     n = int(sda3, 2)
                                                     qqwslenf=len(sda3)
                                                     qqwslenf=(qqwslenf/8)*2
@@ -397,11 +311,14 @@ class compression:
                                                     qqwslenf="%0"+qqwslenf+"x"
                                                     jl=binascii.unhexlify(qqwslenf % n)
                                             #print(jl)
+                                            #print(Times_of_compression1)
 
                                             #print(count4)
                                             if assxw1==50:
-                                                    if Times_of_compression==Times_of_compression1:
-                                                        assxw2=1
+                                                    assxw1=0
+                                                    assxw=0
+                                                    
+                                                    assxw2=1
                                                     if assxw2==1:
                                                         assx=10
                                                         if assx==10:
@@ -479,6 +396,7 @@ class compression:
                             # Read the whole file at once
                             
                             data = binary_file.read()
+
                         
                             s=str(data)
                             lenf1=len(data)
@@ -526,7 +444,7 @@ class compression:
                                     count3+=1
                                     #print(count4)
                                     #######################################################Jurijus Pacalovas Exection Program######################################################################################
-                                    #print(sda2)
+                                    #print(len(sda2))
                                   
                                    
                                     count4=1
@@ -599,116 +517,11 @@ class compression:
                                     
                                     sda2=sda3
                                     assxw=assxw+1
+                                    #print(assxw)
                                     if assxw==50:
-                                        #######################################################Jurijus Pacalovas Exection Program######################################################################################
-                                        #2**32#
-                                        #print(sda2)
-                                        #os.system("pause")
-                                        
-                                        block3=0
-                                        assxw=0
-                                       
-                                        varations=-1
-                                        combinations=-1
-                                        Save_V=""
-                                        Save=""
-                                        V=""
-                                        while varations!=256:
-                                            block3=0
-                                            varations+=1
-                                            Times=0
-                                            while block3<lenf2:
-                                                e4=sda3[block3:block3+8]
-                                                V=format(varations,'08b')
-                                                if e4==V and Times==0:
-                                                    Save_V=Save_V+e4
-                                                    combinations+=1
-                                                    Times=1
-                                                block3+=8
-	                                        
-                                        		                                         
-	                                        		
-	                                        
-                                       	
-                                        	
-
-
-
-                                        Save=format(combinations,'08b')
-                                        
-                                        Save=Save+Save_V
-                                        
-                                        
-                                        #print(len(sda3))
-                                        sda4=""
-
-                                        Save_Long=len(Save)
-                                        combinations1=combinations+1
-                                        #print(combinations)
-                                       
-                                        if combinations>127:
-                                            sda4="11111111"+sda3
-                                        else:
-                                            G=bin(combinations)[2:]
-                                            F=len(G)
-                                            C="0"+str(F)+"b"
-                                            #print(C)
-                                            Bits=0
-                                            block3=0
-                                            sda4=""
-                                            lenf3=len(sda3)
-                                            while block3<lenf3:
-                                                e4=sda3[block3:block3+8]
-                                                e5=""
-                                                
-                                                Bits1=0
-                                                Bits=0
-                                                while e4!=e5:
-                                                    V3=format(Bits,C)
-                                                    e5=Save_V[Bits1:Bits1+8]
-                                                    Bits1+=8
-                                                    Bits+=1
-                                                    
-                                                if e4==e5:
-                                                    sda4=sda4+V3
-                                                    block3+=8
-                                                    #print(block3)
-
-                                        sda4="1"+sda4
-                                        lenf=len(sda4)
-                                        add_bits=""
-                                        count_bits=8-lenf%8
-                                        z=0
-                                        if count_bits!=0:
-                                                if count_bits!=8:
-                                                    while z<count_bits:
-                                                        add_bits="0"+add_bits
-                                                        z=z+1
-                                        sda4=add_bits+sda4
-                                        
-                                                
-                                            
-                                        sda4=Save+sda4
-                                        Save=""
-                                        Save_V=""
-                                        Times_of_compression+=1
-                                        if  len(sda4)>len(sda2):
-                                        	sda4="11111111"+sda3
-
+                                           
+                                        sda4=sda3
                                         #print(len(sda4))
-                                        if len(sda4)>len(sda2) or Times_of_compression==65535:
-                                            Times_count=format(Times_of_compression,'016b')
-                                            count_save=format(count4,'08b')
-                                            sda4=count_save+Times_count+sda4
-                                            assxw1=1
- 
-                                        sda2=sda4
-                                        #print(len(sda4))
-
-                                        
-                                        
-                                            
-                                            	        
                                         n = int(sda4, 2)
                                                                                                     
                                             
@@ -718,6 +531,10 @@ class compression:
                                         qqwslenf="%0"+qqwslenf+"x"
                                          
                                         jl=binascii.unhexlify(qqwslenf % n)
+                                        import paq
+                                        jl= paq.compress(jl)
+                                        assxw1=1
+                                        
           
                                                           
                                  
